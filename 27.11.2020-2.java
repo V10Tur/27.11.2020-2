2aae72499bc46a20cc404289f31a2762af279098
V10Tur <72814308+V10Tur@users.noreply.github.com>
2020-12-02 12:16:41 +0300

    Create 27.11.2020-2.java

---
+++ 27.11.2020-2.java
@@ -0,0 +1,29 @@
+public class Program{
+
+    public static void main(String[] args) {
+
+        Person tom = new Person("Tom");
+        tom.setAccount("qwerty");
+    }
+}
+class Person{
+
+    private String name;
+
+    Person(String name){
+        this.name = name;
+    }
+
+    public void setAccount (String password){
+
+        class Account{
+
+            void display(){
+                System.out.printf("Account Login: %s
+                        \t Password: %s \n", name, password);
+            }
+        }
+        Account account = new Account();
+        account.display();
+    }
+}
